c85279817852605ed1398cacec638be4e894f569
Monil <monil0206@gmail.com>
2018-04-01 19:06:10 +0530

    First Commit

---
+++ app/src/main/java/com/example/monildiwan/smartcity/GActivity.java
@@ -0,0 +1,19 @@
+package com.example.monildiwan.smartcity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class GActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_g);
+
+
+
+    }
+}
